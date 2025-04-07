49b5d2acaab21c96443c52c240e2c23688129a62
DibyaRanjan <mohantydibyaranjan090@gmail.com>
2025-04-07 14:24:36 +0530

    ATM card Logic only showed last 4 Digit

--- DSA/src/com/nt/bankingprojectlogic/ToHideLastDigit.java
+++ DSA/src/com/nt/bankingprojectlogic/ToHideLastDigit.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class ToHideLastDigit {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		System.out.println("enter thr 12 digit number");
+		System.out.println("enter thr 16 digit number");
 	   String CardNumber = sc.nextLine();
-		if( CardNumber.length() ==12) {
-		String maskedCardNumber=	"XXXXXXXX"+CardNumber.substring(8);
+		if( CardNumber.length() ==16) {
+		String maskedCardNumber=	"XXXX XXXX XXXX "+CardNumber.substring(12);
 		System.out.println("masked card number is "+maskedCardNumber);
 
 			
