10e88f7553ce5367e30c1e87dca06717dfbf8556
DibyaRanjan <mohantydibyaranjan090@gmail.com>
2026-04-30 11:43:03 +0530

    Print every third number from 1 to 60 (i.e., 3, 6, 9 …).

--- DSA/src/com/nt/loops/PrintEvery3rdNumber.java
+++ DSA/src/com/nt/loops/PrintEvery3rdNumber.java
@@ -1,6 +1,20 @@
 package com.nt.loops;
 
+import java.util.Scanner;
+
 public class PrintEvery3rdNumber {
-	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the starting number");
+		int fstNumber = sc.nextInt();
+		System.out.println("enter the 2nd number");
+		int endNumber = sc.nextInt();
+		int number = 0;
+		for (int i = fstNumber; i <= endNumber; i++) {
+			i = i + 2;
+			System.out.print(i + ",");
+		}
+
+	}
 
 }
