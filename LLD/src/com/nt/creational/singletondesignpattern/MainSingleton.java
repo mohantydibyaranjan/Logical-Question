fef343943a3a442a10f918de3fce6c4479beb84f
DibyaRanjan <mohantydibyaranjan090@gmail.com>
2026-05-02 00:35:59 +0530

    Updated

--- LLD/src/com/nt/creational/singletondesignpattern/MainSingleton.java
+++ LLD/src/com/nt/creational/singletondesignpattern/MainSingleton.java
@@ -9,3 +9,30 @@ public class MainSingleton {
 	}
 
 }
+/*
+ * 
+ * 🔹 Definition
+ * 
+ * Singleton pattern ensures that only one instance of a class exists in the
+ * entire application and provides a global access point to it.
+ * 
+ * 🔹 Why use it Avoid creating multiple objects unnecessarily Ensure consistent
+ * shared state Save memory/resources 🔹 Where used Logging systems Database
+ * connections Configuration/settings objects Caching Thread pools 🔹 When to
+ * use (conditions)
+ * 
+ * ✔ Only one object is required globally ✔ Object is shared across multiple
+ * classes ✔ Need centralized control (e.g., config, logger)
+ * 
+ * 🔹 When NOT to use
+ * 
+ * ❌ If multiple instances are needed ❌ If it makes testing difficult (tight
+ * coupling)
+ * 
+ * 🔹 Key concept One instance only Global access (getInstance() method) 🔹
+ * One-line interview answer
+ * 
+ * 👉 “Singleton pattern ensures a class has only one instance and provides a
+ * global point of access to it.
+ * 
+ */
