dafd7f88c92ecd5e2336e9990541312ee5f298cf
DibyaRanjan <mohantydibyaranjan090@gmail.com>
2026-05-02 00:32:49 +0530

    Updated

--- LLD/src/com/nt/creational/prototypedesignpattern/MainPrototype.java
+++ LLD/src/com/nt/creational/prototypedesignpattern/MainPrototype.java
@@ -12,3 +12,35 @@ public class MainPrototype {
 	}
 
 }
+/*
+ * 
+ * 
+ * 
+ * 🔹 Definition
+ * 
+ * Prototype pattern creates new objects by cloning an existing object instead
+ * of using new.
+ * 
+ * 🔹 Why use it Avoid costly object creation (DB calls, heavy setup) Reduce
+ * repetitive code Get same data + independent objects 🔹 Where used Templates
+ * (user settings, documents) Game objects (enemy cloning) Spring prototype
+ * beans Undo/backup systems 🔹 When to use
+ * 
+ * ✔ Object creation is expensive ✔ Need many similar objects ✔ Want independent
+ * copies (change one → others not affected)
+ * 
+ * 🔹 When NOT to use
+ * 
+ * ❌ Object is simple ❌ Cloning is complex
+ * 
+ * 🔹 Key concept Same data initially Different objects in memory 🔹 One-line
+ * interview answer
+ * 
+ * 
+ * “Prototype pattern creates objects by cloning an existing instance to improve
+ * performance and allow independent copies.”
+ * 
+ * 
+ * 
+ * 
+ */
