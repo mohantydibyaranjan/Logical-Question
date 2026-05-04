1cd3677199a90729ee6bc77a8bcc32a42fdd10a3
DibyaRanjan <mohantydibyaranjan090@gmail.com>
2026-05-04 16:10:07 +0530

    Repeating and non Repeating

--- DSA/src/com/nt/list/NonRepeatingElement.java
+++ DSA/src/com/nt/list/NonRepeatingElement.java
@@ -2,11 +2,14 @@ package com.nt.list;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class NonRepeatingElement {
 	public static void main(String[] args) {
 		List<Integer> input = Arrays.asList(1, 2, 2, 3, 4, 3, 5);
+		Set<Integer> ss = new HashSet<Integer>();
 		List<Integer> nonRepeatingOnes = new ArrayList<>();
 
 		// Integer noReapting = null;
@@ -17,7 +20,18 @@ public class NonRepeatingElement {
 			}
 
 		}
+
+		System.out.println("Repated");
 		System.out.println(nonRepeatingOnes);
+
+		for (Integer num : input) {
+			if (input.indexOf(num) != input.lastIndexOf(num)) {
+				ss.add(num);
+
+			}
+
+		}
+		System.out.println(ss);
 
 	}
 
