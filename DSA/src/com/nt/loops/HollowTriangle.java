69d6be1b6a2485bed38a221c7cbb13c48be20016
DibyaRanjan <mohantydibyaranjan090@gmail.com>
2026-05-05 23:00:14 +0530

    Hollow Triangle

---
+++ DSA/src/com/nt/loops/HollowTriangle.java
@@ -0,0 +1,32 @@
+
+package com.nt.loops;
+
+/*
+ *    
+ * 
+ * Print a hollow triangle of stars (only the border of the triangle is stars).
+ */
+
+//*
+//**
+//* *
+//*  *
+//*****
+public class HollowTriangle {
+	public static void main(String[] args) {
+		int n = 5;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				if (j == 1 || j == i || i == n) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+
+			}
+			System.out.println();
+		}
+
+	}
+
+}
