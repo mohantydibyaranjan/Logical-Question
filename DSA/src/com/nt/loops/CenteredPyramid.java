81f778e7ec3798f974ed9550168a7d3559557be9
DibyaRanjan <mohantydibyaranjan090@gmail.com>
2026-05-05 09:57:27 +0530

    centred pyramid of stars

---
+++ DSA/src/com/nt/loops/CenteredPyramid.java
@@ -0,0 +1,22 @@
+package com.nt.loops;
+/*
+ * 
+ * Print a centred pyramid of stars for n rows
+ */
+
+public class CenteredPyramid {
+	public static void main(String[] args) {
+		int n = 5;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= (2 * i - 1); j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
