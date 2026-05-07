21c5a4857a7fe4e22035df24ca6cbe0dddc47d5a
DibyaRanjan <mohantydibyaranjan090@gmail.com>
2026-05-07 12:34:49 +0530

    fix: print all number pairs whose sum equals 10 using nested loops

--- DSA/src/com/nt/loops/PrintAllPairs.java
+++ DSA/src/com/nt/loops/PrintAllPairs.java
@@ -6,8 +6,9 @@ package com.nt.loops;
  */
 public class PrintAllPairs {
 	public static void main(String[] args) {
-		for (int i = 1; i <= 9; i++) {
-			for (int j = 1; j <= 9; j++) {
+		int n = 9;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
 				if (i + j == 10) {
 					System.out.println(i + " " + j);
 				}
