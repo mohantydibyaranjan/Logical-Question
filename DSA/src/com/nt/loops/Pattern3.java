32dbc1ff3c2e26f03f0f78e6fd9a3a24c510b512
DibyaRanjan <mohantydibyaranjan090@gmail.com>
2026-05-07 12:21:17 +0530

    updated pattern

--- DSA/src/com/nt/loops/Pattern3.java
+++ DSA/src/com/nt/loops/Pattern3.java
@@ -9,9 +9,10 @@ package com.nt.loops;
 */
 public class Pattern3 {
 	public static void main(String[] args) {
-		for (int i = 1; i >= 1; i++) {
-			for (int j = i; j >= i; j++) {
-				System.out.print(j + "");
+		int n = 5;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i * 1; j++) {
+				System.out.print(i + "");
 			}
 			System.out.println();
 		}
