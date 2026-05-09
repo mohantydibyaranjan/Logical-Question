b15cfa3442b65812dfed37c8a4484c1356949846
DibyaRanjan <mohantydibyaranjan090@gmail.com>
2026-05-09 19:54:18 +0530

    Reverse Array

---
+++ DSA/src/com/nt/dsapattern/twopointer/ReverseArray.java
@@ -0,0 +1,47 @@
+package com.nt.dsapattern.twopointer;
+
+import java.util.Scanner;
+
+/*
+ * 
+ * 
+ * Reverse an Array Using Two Pointers
+Example
+[1,2,3,4]
+
+Output:
+
+[4,3,2,1]
+ */
+
+public class ReverseArray {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the Array size");
+		int size = sc.nextInt();
+		int arr[] = new int[size];
+		System.out.println("entre the Array element");
+		for (int i = 0; i < arr.length - 1; i++) {
+			arr[i] = sc.nextInt();
+
+		}
+		System.out.println(ReverseArray.reverse(arr));
+
+	}
+
+	public static int[] reverse(int arr[]) {
+		int left = 0;
+		int right = arr.length - 1;
+		while (left < right) {
+			int reverse = arr[left];
+			arr[left] = arr[right];
+			arr[right] = reverse;
+			left++;
+			right--;
+		}
+
+		return arr;
+
+	}
+
+}
