f3c8a9ce70a4b3116241f8f78a314f1e0225f403
DibyaRanjan <mohantydibyaranjan090@gmail.com>
2026-05-18 13:21:40 +0530

    Second Largest Number

--- DSA/src/com/nt/array/SecondLargestWithoutSorting.java
+++ DSA/src/com/nt/array/SecondLargestWithoutSorting.java
@@ -1,5 +1,42 @@
 package com.nt.array;
 
+import java.util.Scanner;
+
 public class SecondLargestWithoutSorting {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the array size");
+		int length = sc.nextInt();
+		int[] arr = new int[length];
+		System.out.println("enter the Array element");
+		for (int i = 0; i <= arr.length - 1; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int secondLargest = SecondLargestWithoutSorting.secondLargest(arr);
+		System.out.println(secondLargest);
+		sc.close();
+
+	}
+
+	public static int secondLargest(int[] arr) {
+
+		int largest = Integer.MIN_VALUE;
+		int secondLargest = Integer.MIN_VALUE;
+
+		for (int num : arr) {
+
+			if (num > largest) {
+
+				secondLargest = largest;
+				largest = num;
+
+			} else if (num > secondLargest && num != largest) {
+
+				secondLargest = num;
+			}
+		}
+
+		return secondLargest;
+	}
 
 }
