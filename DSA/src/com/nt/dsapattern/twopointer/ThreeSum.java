e7e45ea4c4dca5021bf283a62c23f425f0b3cf27
DibyaRanjan <mohantydibyaranjan090@gmail.com>
2026-05-18 14:43:26 +0530

    Three Sum Update Test cases

--- DSA/src/com/nt/dsapattern/twopointer/ThreeSum.java
+++ DSA/src/com/nt/dsapattern/twopointer/ThreeSum.java
@@ -78,7 +78,7 @@ public class ThreeSum {
 					while (left < right && nums[left] == nums[left - 1]) {
 						left++;
 					}
-					while (left < right && nums[right] == nums[right - 1]) {
+					while (left < right && nums[right] == nums[right + 1]) {
 						right--;
 					}
 				} else if (sum < 0) {
